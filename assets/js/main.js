8c7e477ce86ec2aee5931bda6b62ebc09662b649
abhijeetmohol <abhijeetmohol3101@gmail.com>
2023-04-27 15:56:30 +0530

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,112 @@
+console.log("js-comparision-operators")
+
+//equal to
+console.log("1.equal to : ")
+let a=20;
+let b=30;
+console.log("a=20");
+console.log("b=30");
+console.log("a==b : ",a==b);
+
+//not equal to
+console.log("2.not equal to : ")
+let c=50;
+let d=80;
+console.log("c=50");
+console.log("d=80");
+console.log("a!=b : ",a!=b);
+
+//equal value and same type
+console.log("3.equal value and same data type")
+let e="abhijeet";
+let f=50;
+console.log("e=abhijeet")
+console.log("f=50")
+console.log("e===f : ",e===f)
+
+//not equal value and same type
+console.log("4.not equal value or no same data type")
+let g="50";
+let h=50;
+console.log("g=50")
+console.log("h=50")
+console.log("g!==h : ",g!==h)
+
+//less than
+console.log("5.less than : ")
+let i=50;
+let j=80;
+console.log("i=50");
+console.log("j=80");
+console.log("i<j : ",i<j);
+
+//greater than
+console.log("6.greater than : ")
+let k=50;
+let l=80;
+console.log("k=50");
+console.log("l=80");
+console.log("k>l : ",k>l);
+
+//greater than equal to
+console.log("7.greater than equal to : ")
+let m=50;
+let n=80;
+console.log("m=50");
+console.log("n=80");
+console.log("m>=n : ",m>=n);
+
+//less than equal to
+console.log("8.less than equal to : ")
+let o=50;
+let p=80;
+console.log("o=50");
+console.log("p=80");
+console.log("o<=p : ",o<=p);
+
+//ternary operator
+console.log("9.ternary to : ")
+let r=50;
+let s=80;
+console.log("r=50");
+console.log("s=80");
+console.log("r?s : ",);
+
+
+//data types 
+console.log("            Pre-mative data types")
+//string
+console.log("string")
+let aa="Abhijeet"
+console.log(aa)
+console.log(typeof aa,"is the type of",aa)
+
+//number
+let bb=5050;
+console.log(bb)
+console.log(typeof bb,"is the type of",bb)
+
+//symball
+console.log("symball")
+
+//undefine
+console.log("undefine")
+let cc
+console.log(typeof cc,"is the type of")
+
+//null
+console.log("null")
+console.log("null returns object")
+let dd=null;
+console.log(typeof dd)
+
+//bigint
+console.log("bigint")
+let ee=123456n;
+console.log(ee)
+console.log(typeof ee,"is the type of", ee)
+
+
+console.log("boolean")
+let xyz=10 > 9
+console.log("xyz=10>9",typeof xyz ,xyz)
